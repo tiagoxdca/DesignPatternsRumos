4a68ed9ad5dabd6083bb36223ddf45f088f9668c
Tiago <soares343vio@hotmail.com>
2017-09-27 01:12:31 +0100

    Initial commit

---
+++ src/test/java/singleton/SingletonTest.java
@@ -0,0 +1,19 @@
+package singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SingletonTest {
+
+
+    @Test
+    public void singletonInstanceAreEqual(){
+
+        SingleObject object = SingleObject.getInstance();
+
+        SingleObject object1 = SingleObject.getInstance();
+
+        Assert.assertEquals(object, object1);
+
+    }
+}
